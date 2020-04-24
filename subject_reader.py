8349ee27f19212d0df2ed7a0cfddd6b76de70247
Yueran <yueran.wu@my.jcu.edu.au>
2020-04-25 02:29:05 +0800

    Add final code of data string to list conversion program

--- subject_reader.py
+++ subject_reader.py
@@ -8,12 +8,14 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    for line in data:
+        print("{} is taught by {:<12} and has {:>3} students".format(line[0], line[1], line[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []   # create a new empty list called data
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +25,9 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)  # Add line list into a list
     input_file.close()
+    return data
 
 
 main()
